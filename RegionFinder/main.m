da83b1e3a2a2d2c8e10492c975ee39e9615af52d
Andrew Erickson <aerickson14@gmail.com>
2014-08-08 18:17:08 -0500

    Initial Commit

---
+++ RegionFinder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RegionFinder
+//
+//  Created by Andrew Erickson on 2014-08-08.
+//  Copyright (c) 2014 Andrew Erickson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
